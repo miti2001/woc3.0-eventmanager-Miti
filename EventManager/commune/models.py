1602a1f2471577093c50c89e05acaaf07d0c60ca
miti2001 <71545727+miti2001@users.noreply.github.com>
2021-01-07 20:21:41 +0530

    Added home page and Event Registration model

--- EventManager/commune/models.py
+++ EventManager/commune/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class Event (models.Model):
+    EventName = models.CharField(max_length = 32)
+    Desc = models.TextField(blank = True, max_length = 50)
+    Loc = models.CharField(max_length = 32)
+    FromDate = models.DateField(default = datetime.date.today)
+    FromTime = models.TimeField(default = timezone.now)
+    ToDate = models.DateField(default = datetime.date.today)
+    ToTime = models.TimeField(default = timezone.now)
+    RegEndDate = models.DateField(default = datetime.date.today)
+    RegEndTime = models.TimeField(default = timezone.now)
+    HostEmail = models.EmailField(max_length = 254)
+    HostPassword = models.CharField(max_length = 32)
+    Status = models.IntegerField()
+
+    def __str__ (self):
+        return self.EventName
--- EventManager/commune/views.py
+++ EventManager/commune/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django import forms
+from .forms import EventForm
+from .models import Event
+
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def eventReg(request):
+    if request.method == 'GET':
+        form = EventForm
+        return render(request, 'eventReg.html', {
+            'form':form,
+    })
+
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Saved')
